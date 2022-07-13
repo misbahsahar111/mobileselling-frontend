dd97d5a3bddc0683bb3f415110ea010fe3d5f83d
Misbah Sahar <misbahsahar08@gmail.com>
2022-07-13 16:06:00 -0400

    added about and contact page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   root "articles#index"
-  get "/articles", to: "articles#index"
+  get "about", to: "articles#about"
+  get "contact", to: "articles#contact"
+  resources :articles
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
