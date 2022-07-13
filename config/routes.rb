4a26543e513499f114fbeb0ac7768d3870c5d7e3
Misbah Sahar <misbahsahar08@gmail.com>
2022-07-13 16:38:38 -0400

    improved sidebar

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root "articles#index"
   get "about", to: "articles#about"
   get "contact", to: "articles#contact"
+  get "shop", to: "articles#shop"
   resources :articles
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
